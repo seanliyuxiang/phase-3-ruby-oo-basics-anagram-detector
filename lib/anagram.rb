57bca509aa4a9864e4aa5a70e21e7d042df891d8
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-14 09:45:23 -0500

    commit tree bfa1ca89b9c9d74dbc9877a16b4f754f31762b6f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_arr)
+    # iterate through the words array
+    # if the word is an anagram of the initialized word, select it into a new array
+    # return that new array
+    words_arr.select do |single_word|
+      # split the two words into arrays and compare them
+      single_word.chars.sort == @word.chars.sort
+    end
+  end
+
+end
